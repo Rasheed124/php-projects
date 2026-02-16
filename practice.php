489ec66362750a11791dbf5a1ea452b61a25734f
Rasheed124 <rashdevatrealglobe@gmail.com>
2026-02-17 00:13:04 +0100

    working on array nesting

--- practice.php
+++ practice.php
@@ -31,26 +31,65 @@ Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam odio corrupt
 
     // var_dump($splitted);
 
+    $students = [
+    [
+        "id"      => 1,
+        "name"    => "John Doe",
+        "courses" => [
+            [
+                "course_id"   => "CS101",
+                "course_name" => "Computer Science",
+                "grade"       => "A",
+            ],
+            [
+                "course_id"   => "MATH201",
+                "course_name" => "Mathematics",
+                "grade"       => "B+",
+            ],
+        ],
+    ],
+    [
+        "id"      => 2,
+        "name"    => "Jane Smith",
+        "courses" => [
+            [
+                "course_id"   => "ENG101",
+                "course_name" => "English Literature",
+                "grade"       => "A-",
+            ],
+            [
+                "course_id"   => "PHY101",
+                "course_name" => "Physics",
+                "grade"       => "B",
+            ],
+        ],
+    ],
+    ];
+
 ?>
 
 
 
-<ul>
+<?php foreach ($students as $student): ?>
+    <h2><?php echo $student['name'] ?></h2>
+
+    <?php foreach ($student['courses'] as $courses): ?>
+        <p><?php echo $courses['course_name'] ?></p>
+    <?php endforeach?>
+    <?php endforeach?>
+
+    <!-- <table border="">
+
+    <tr>
+        <th>Name</th>
+    </tr>
+    <tr>
+        <th>Course</th>
+    </tr>
+
+    </table> -->
 
 
- 
-
-</ul>
-
-
-
-
-
-<p>
-
-<?php echo str_replace('.', '</p><p>', $text) ?>
-
-</p>
 
 
 
