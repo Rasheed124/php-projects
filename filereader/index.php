9b2dd769610ae4e40de2773296db9ac75bdda8be
Rasheed124 <rashdevatrealglobe@gmail.com>
2026-02-19 18:00:15 +0100

    completed file uploads

--- filereader/index.php
+++ filereader/index.php
@@ -4,12 +4,23 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+
+    <style>
+        img{
+            width: 400px;
+            height: 400px;
+        }
+    </style>
 </head>
 <body>
 <pre>
 <?php
 
     $getFIlePath = pathinfo('index.php', PATHINFO_EXTENSION);
+    function e($value)
+    {
+    return htmlspecialchars($value, ENT_QUOTES, 'UTF-8');
+    }
 
     // var_dump($getFIlePath);
 
@@ -18,55 +29,68 @@
 
     $isfolderExist = is_dir(__DIR__ . '/images');
 
-    if ($isfolderExist) {
-
     $handle            = opendir(__DIR__ . '/images');
     $allowedExtensions = [
-        'jpg',
-        'png'
+    'jpg',
+    'png',
     ];
     $textContents = [];
     $contents     = [];
 
     $images = [];
     while (($currentFile = readdir($handle)) !== false) {
-        if ($currentFile === '.' || $currentFile === '..') {
-            continue;
-        }
-        $contents[] = $currentFile;
+    if ($currentFile === '.' || $currentFile === '..') {
+        continue;
+    }
+    $filePath = pathinfo($currentFile, PATHINFO_EXTENSION);
+
+    if (! in_array($filePath, $allowedExtensions)) {
+        continue;
+    }
+
+    $fileWithouExt = pathinfo($currentFile, PATHINFO_FILENAME);
+    $txtFile       = __DIR__ . '/images/' . $fileWithouExt . '.txt';
+    $title         = '';
+    $content       = [];
+    if (file_exists($txtFile)) {
+        $txt            = file_get_contents($txtFile);
+        $array_form_txt = explode("\n", $txt);
+
+        $title = $array_form_txt[0];
+        unset($array_form_txt[0]);
+        // var_dump($array_form_txt);
+        $content = array_values($array_form_txt);
+    }
+
+    $images[] = [
+        'images'  => $currentFile,
+        'title'   => $title,
+        'content' => $content,
+    ];
     }
     closedir($handle);
 
-    foreach ($contents as $textContent) {
-        $filePath = pathinfo($textContent, PATHINFO_EXTENSION);
-
-        if ($filePath === 'txt') {
-            $textContents[] = file_get_contents("images/$textContent");
-        }
-    }
-
-    foreach ($contents as $imageContent) {
-        $filePath = pathinfo($imageContent, PATHINFO_EXTENSION);
-
-        if (! in_array($filePath, $allowedExtensions)) {
-            continue;
-        }
-
-        $images[] = $imageContent;
-    }
-
-    }
+    // var_dump($images);
 
 ?>
 </pre>
 
 
 <?php foreach ($images as $image): ?>
-    <img src="images/<?php echo rawurlencode($image) ?>" alt="">
+    <figure>
+
+
+    </figure>
+    <p><?php echo e($image['title']) ?></p>
+    <img src="images/<?php echo rawurlencode($image['images']) ?>" alt="">
+    
+    <?php foreach ($image['content'] as $contentParagraph): ?>
+       <figcaption>
+               <p><?php echo e($contentParagraph) ?></p>
+       </figcaption>
+    <?php endforeach; ?>
 <?php endforeach; ?>
-<?php foreach ($textContents as $text): ?>
-    <p><?php echo $text ?></p>
-<?php endforeach; ?>
+
 
 </body>
 </html>
