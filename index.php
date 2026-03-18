6f8e7a68dbda15e1a41e402647403634bd62ebd4
Rasheed124 <rashdevatrealglobe@gmail.com>
2026-03-18 13:06:47 +0100

    working on file strcuture

--- index.php
+++ index.php
@@ -1,9 +1,16 @@
 <?php
 require __DIR__ . '/inc/all.inc.php';
 
+use \App\blogFrontend\Controller\PagesController;
 
+$route = @(string) ($_GET['route'] ?? 'pages');
 
+if ($route === 'pages') {
+    $page = @(string) ($_GET['page'] ?? 'index');
 
+   $pagesController = new PagesController();
+   $pagesController->index();
 
-
-
+} else if ($route === 'admin/pages') {
+    echo "Admin Page";
+}
---
+++ src/blogFrontend/Controller/PagesController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\blogFrontend\Controller;
+
+
+class PagesController
+{
+
+    public function index()
+    {
+        echo "Home Page";
+    }
+}
